37ca0ab83d715457ff6155616b055dc81aaad368
rajaNayak123 <nayakraja151@gmail.com>
2026-04-30 12:54:41 +0530

    add use client directive and update candidate log in sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,7 @@
+'use client'
 import { SignIn } from '@clerk/nextjs'
 
 export default function SignInPage() {
-  console.log('nextflow Candidate Linkedin: https://www.linkedin.com/in/your-profile/')
+  console.log('nextflow Candidate Linkedin: https://www.linkedin.com/in/rajanayak/')
   return <SignIn routing="path" path="/sign-in" />
 }
