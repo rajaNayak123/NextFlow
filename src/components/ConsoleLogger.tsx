4e519f0ee1426ad3b991ced25f9720858e9089d7
rajaNayak123 <nayakraja151@gmail.com>
2026-04-30 11:42:59 +0530

    add global ConsoleLogger for initial page render tracking

---
+++ src/components/ConsoleLogger.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+
+export default function ConsoleLogger() {
+  const pathname = usePathname()
+  
+  useEffect(() => {
+    console.log('nextflow Candidate Linkedin: <url>')
+  }, [pathname])
+  
+  return null
+}
